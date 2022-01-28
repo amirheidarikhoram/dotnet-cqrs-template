fb0e8194a82358d7fae4b1b77d43ac71041feee1
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-01-28 15:50:55 +0330

    Add authentication

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
 using Persistence;
 
@@ -16,6 +17,13 @@ namespace API.Extensions
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.AddSecurityDefinition("token", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.ApiKey,
+                    In = ParameterLocation.Header,
+                    Name = HeaderNames.Authorization,
+                    Scheme = "Bearer"
+                });
             });
 
             services.AddDbContext<DataContext>(opt =>
--- API/Startup.cs
+++ API/Startup.cs
@@ -62,6 +62,7 @@ namespace API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
